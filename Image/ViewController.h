5cd4100c09b647cf93a4d916aeb6b5b741d4a185
smart_small <smart_small@163.com>
2017-04-24 11:25:29 +0800

    Initial Commit

---
+++ Image/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Image
+//
+//  Created by goldeneye on 2017/4/24.
+//  Copyright © 2017年 goldeneye by smart-small. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Image/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Image
+//
+//  Created by goldeneye on 2017/4/24.
+//  Copyright © 2017年 goldeneye by smart-small. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
